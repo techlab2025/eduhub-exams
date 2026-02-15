dfcfd57d1f11665284863f45b86e1a95a75849e5
Sayed2210 <eh600039@gmail.com>
2026-02-15 12:42:21 +0200

    update layout

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import { createPinia } from "pinia";
+import router from "./router";
 import "./style.css";
 import App from "./App.vue";
-createApp(App).use(PrimeVue).use(createPinia()).mount("#app");
+
+createApp(App).use(PrimeVue).use(createPinia()).use(router).mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import { organizationRoutes } from './routes/organization'
 import { sharedRoutes } from './routes/shared'
 import { addSuffix } from './helpers'
-import { authGuard } from './guards'
 import { dashboardRoutes } from './routes/modules'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
+
     {
       path: '/',
       name: 'Organization App',
-      component: () => import('../App.vue'),
+      component: () => import('../views/AppLayout.vue'),
       children: [
         ...dashboardRoutes,
         ...addSuffix(sharedRoutes, 'Shared'),
       ],
-      
+
     },
   ],
 })
