79b847d2908c7a2d8c6bdc43e1fb94a2e5a898a3
MohabMohamed72 <bhoba17@gmail.com>
2026-05-01 00:40:49 +0300

    update login form

--- src/modules/auth/presentation/components/LoginForm.vue
+++ src/modules/auth/presentation/components/LoginForm.vue
@@ -1,77 +1,67 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import LoginController from '../controllers/login.controller';
-  import LoginParams from '../../core/params/login.params';
-  // import logo from '@/assets/images/TechlabLogo.png';
-  import CloseEyeIcon from '@/shared/icons/Login/CloseEyeIcon.vue';
-  import OpenEyeIcon from '@/shared/icons/Login/OpenEyeIcon.vue';
+import { ref } from 'vue';
+import LoginController from '../controllers/login.controller';
+import LoginParams from '../../core/params/login.params';
+import logo from '@/assets/images/TechlabLogo.png';
+import CloseEyeIcon from '@/shared/icons/Login/CloseEyeIcon.vue';
+import OpenEyeIcon from '@/shared/icons/Login/OpenEyeIcon.vue';
+// import ChangeLanguage from '@/shared/LayoutComponents/SubComponents/ChangeLanguage.vue';
 
-  const email = ref('');
-  const password = ref('');
+const email = ref('');
+const password = ref('');
 
-  const controller = LoginController.getInstance();
+const controller = LoginController.getInstance();
 
-  const login = async () => {
-    const params = new LoginParams(email.value, password.value);
-    await controller.login(params);
-  };
+const login = async () => {
+  const params = new LoginParams(email.value, password.value);
+  await controller.login(params);
+};
 
-  const isPasswordVisible = ref();
+const isPasswordVisible = ref(false);
 </script>
 
 <template>
   <section class="login">
-    <!-- <div class="card">
-      <div class="login-logo">
-        <img :src="logo" width="22" alt="logo" class="logo" />
-      </div>
-    </div> -->
+    <div class="login-header">
+      <img :src="logo" alt="TechLab" class="logo-img" />
+    </div>
 
     <form class="login-form" @submit.prevent="login">
-      <div class="title">
-        <div class="logo">
-          <h2>Welcome Back</h2>
-          <p>Access your dashboard to manage platform content and data</p>
-        </div>
+      <div class="lang-switcher">
+        <!-- <ChangeLanguage /> -->
       </div>
+
+      <div class="title">
+        <h2>{{ $t('Welcome Back') }}</h2>
+        <p>{{ $t('Access your dashboard to manage platform content and data') }}</p>
+      </div>
+
       <div class="inputs">
         <div class="input-wrapper">
           <label for="email">Email</label>
-          <input
-            id="email"
-            v-model="email"
-            class="input"
-            :placeholder="$t('Enter Your Mail')"
-            type="email"
-          />
+          <input id="email" v-model="email" class="input" :placeholder="$t('Enter Your Mail')" type="email" />
         </div>
         <div class="input-wrapper">
-          <label for="password">password</label>
-          <input
-            id="password"
-            v-model="password"
-            :type="isPasswordVisible ? 'text' : 'password'"
-            :placeholder="$t('Enter Password')"
-            class="input py"
-          />
-          <CloseEyeIcon
-            v-if="isPasswordVisible"
-            class="icon-eye"
-            @click="isPasswordVisible = !isPasswordVisible"
-          />
+          <label for="password">{{ $t('Password') }}</label>
+          <input id="password" v-model="password" :type="isPasswordVisible ? 'text' : 'password'"
+            :placeholder="$t('Enter Your Password')" class="input py" />
+          <CloseEyeIcon v-if="isPasswordVisible" class="icon-eye" @click="isPasswordVisible = !isPasswordVisible" />
           <OpenEyeIcon v-else class="icon-eye" @click="isPasswordVisible = !isPasswordVisible" />
         </div>
       </div>
-      <button type="submit" class="btn btn-primary">{{ $t('login') }}</button>
+
+      <button type="submit" class="btn btn-primary">{{ $t('Log In') }}</button>
     </form>
   </section>
 </template>
 
 <style scoped>
-  .login {
-    background-image: url('../../../../assets/images/LoginBg.png');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
+.login {
+  background-image: url('../../../../assets/images/LoginBg.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+
 </style>
